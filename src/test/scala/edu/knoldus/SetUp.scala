630a8a921f672c20808701143c0bc1ebaeb8b1b3
rehlangitika <gitika.rehlan@knoldus.in>
2017-03-29 17:01:34 +0530

    changes made

--- src/test/scala/edu/knoldus/SetUp.scala
+++ src/test/scala/edu/knoldus/SetUp.scala
@@ -8,12 +8,12 @@ trait SetUp {
 
   val BASE_URL = "https://www.flipkart.com/"
   val CHECKOUT_URL = "https://www.flipkart.com/checkout/init"
-  val PHONE = "9999747608"
-  val password = "gitika31"
-  val pincode = "110063"
-  val NAME = "Gitika Rehlan"
-  val address = "B-3/341, Paschim Vihar"
-  val landmark = "Near BSES Office"
+  val PHONE = "***********"
+  val password = "*******"
+  val pincode = "*****"
+  val NAME = "*********"
+  val address = "*****************"
+  val landmark = "*************"
   System.setProperty("webdriver.chrome.driver", "/home/gitika/Downloads/chromedriver/chromedriver")
   val capabilities = DesiredCapabilities.chrome()
   val driver = new ChromeDriver(capabilities)
